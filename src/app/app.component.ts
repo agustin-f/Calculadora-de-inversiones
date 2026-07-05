2f0093e06533404e9bdcea5b7b879ca2683dd443
Agustin Jose Farisano <farisanoagustin@gmail.com>
2026-07-05 14:20:59 -0300

    muestro tabla de resultados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,28 @@ import { Component } from '@angular/core';
 import { EncabezadoComponent } from './encabezado/encabezado.component';
 import { IngresoUsuarioComponent } from './ingreso-usuario/ingreso-usuario.component';
 import { ingresoInversion } from './ingreso-inversion-model';
+import { ResultadosInversionComponent } from './resultados-inversion/resultados-inversion.component';
 
 @Component({
   selector: 'app-raiz',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [EncabezadoComponent, IngresoUsuarioComponent],
+  imports: [
+    EncabezadoComponent,
+    IngresoUsuarioComponent,
+    ResultadosInversionComponent,
+  ],
 })
 export class AppComponent {
+  infoResultados?: {
+    anio: number;
+    interes: number;
+    valorFinalAnio: number;
+    inversionAnual: number;
+    interesTotal: number;
+    montoTotalInvertido: number;
+  }[];
+
   alCalcularResultadosInversion(info: ingresoInversion) {
     const { inversionInicial, inversionAnual, rendimientoEsperado, duracion } =
       info;
@@ -32,6 +46,6 @@ export class AppComponent {
       });
     }
 
-    console.log(datosAnuales);
+    this.infoResultados = datosAnuales;
   }
 }
