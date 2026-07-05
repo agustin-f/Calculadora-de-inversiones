a0bee3821cb4724acec0b28b920abd5685bbce4e
Agustin Jose Farisano <farisanoagustin@gmail.com>
2026-07-05 12:17:06 -0300

    agrego componente encabezado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
 
 @Component({
   selector: 'app-raiz',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [EncabezadoComponent],
 })
 export class AppComponent {}
